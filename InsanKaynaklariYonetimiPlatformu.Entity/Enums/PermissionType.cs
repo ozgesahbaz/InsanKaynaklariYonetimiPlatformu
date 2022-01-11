8e5ad4105661069deb7d7cc5e431832a37e9bbdb
Irem Yurtlugil <irem.yurtlugil@bilgeadamboost.com>
2022-01-11 18:16:21 +0300

    Entity ve Enums

---
+++ InsanKaynaklariYonetimiPlatformu.Entity/Enums/PermissionType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanKaynaklariYonetimiPlatformu.Entity.Enums
+{
+    public enum PermissionType
+    {
+        YıllıkIzın = 1,
+        DoğumIznı,
+        BabalıkIznı,
+        UcretsızIzın
+    }
+}
---
+++ InsanKaynaklariYonetimiPlatformu.Entity/Permissions.cs
@@ -0,0 +1,17 @@
+﻿using InsanKaynaklariYonetimiPlatformu.Entity.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanKaynaklariYonetimiPlatformu.Entity
+{
+   public class Permissions
+    {
+        public int PermissionId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+        public PermissionType PermissionType { get; set; }
+    }
+}
