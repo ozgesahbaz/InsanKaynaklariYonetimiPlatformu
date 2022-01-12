25ba5fc1855dcf39fa7b71db44809420756ca6e9
özge şahbaz <ozgeesahbazz@gmail.com>
2022-01-12 15:05:38 +0300

    Register Get ve Post methodları oluşturuldu. yapılacak işlemler not düşüldü.

--- InsanKaynaklariYonetimiPlatformu/Controllers/ManagerController.cs
+++ InsanKaynaklariYonetimiPlatformu/Controllers/ManagerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InsanKaynaklariYonetimiPlatformu.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,12 +18,42 @@ namespace InsanKaynaklariYonetimiPlatformu.UI.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(ManagerLoginVM manager)
+        {
+            //loginvm gelecek ve isvalid kontrolü yapılacak
+             //böyle bir kullanıcı var mı konrolü yapılacak
+             //kullanıcının company isapprovali true mu kontrolü yapılacak
+             //false ise kullanıcıya önce şirket mailinden doğrulama yapılmalıdır uyarısı verilecek
+             //true ise kullanıcı is active mi kontrolü yapılacak
+             //true ise kullanıcı managerindex'e yönlendirilecek
+             //false ise kullanıcıya uyarı gönderilecek
+            return View();
+        }
 
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
-
+        [HttpPost]
+        public IActionResult Register(ManagerRegisterVM register)
+        {
+            //Vm buraya gelecek isvalid kontrolü yapılacak
+            //Böyle bir kullanıcı zaten var mı kontrolü yapılacak
+            //Company ve manager oluşturulacak anca manager is active=false, company isapproval=false olarak
+            //Şirket mailine doğrulama maili atılacak (Şifre veya link olarak? link olursa yanına mutlaka şirket ıdsi eklenecek)
+            //Şirket mailine giden kodu onaylayınız sayfası gelecek.
+            
+            return View();
+        }
 
     }
 }
