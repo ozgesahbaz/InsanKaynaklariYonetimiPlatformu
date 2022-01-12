da353053f87bc297fe4b48c7c2567b3bc0471793
Caner KALE <caner_kale_14@hotmail.com>
2022-01-12 18:35:03 +0300

    DbContext olustruldu ve  conntectin string  yolu açılmak uzere hazırlandı

---
+++ InsanKaynaklariYonetimiPlatformu.Entity/HRContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanKaynaklariYonetimiPlatformu.Entity
+{
+    public class HRContext : DbContext
+    {
+        public HRContext(DbContextOptions<HRContext> options) : base(options)
+        {
+#if DEBUG
+
+#endif
+            if (Database.GetPendingMigrations().Count()>0)
+            {
+                Database.Migrate();
+            }
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("name = ConnectionStrings:DefaultConnection");
+            }
+
+          
+        }
+    }
+}
