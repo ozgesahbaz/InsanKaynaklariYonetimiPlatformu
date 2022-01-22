e518478526628ceb4064ee8242169a745694ac53
Caner KALE <caner_kale_14@hotmail.com>
2022-01-22 10:58:13 +0300

    shift ve respit vm birleştiirldi.

---
+++ InsanKaynaklariYonetimiPlatformu.ViewModels/ManagerVM/ShiftRespiteVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanKaynaklariYonetimiPlatformu.ViewModels.ManagerVM
+{
+   public class ShiftRespiteVM
+    {
+        public ShiftRespiteVM()
+        {
+            List<ShiftRespiteVM> Respites = new List<ShiftRespiteVM>();
+        }
+        public DateTime ShiftStartTime { get; set; }
+        public DateTime ShiftFinishTime { get; set; }
+        public int? ShiftId { get; set; }
+        public int EmployedId { get; set; }
+        public DateTime RespiteStartTime { get; set; }
+        public DateTime RespiteFinishTime { get; set; }
+
+        public List<ShiftRespiteVM> ShiftRespiteVMs { get; set; }
+    }
+}
